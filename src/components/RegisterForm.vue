2f09def277de8f17abaa80cda1344f90d2f94db3
Mohamed Omar <mohamed.omar.mubark@gmail.com>
2022-10-26 22:10:40 +0200

    feat (Auth): handling the register response

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -141,13 +141,21 @@ export default {
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_msg = "Please wait... Your account is being created.";
 
-      const userCred = await firebase
-        .auth()
-        .createUserWithEmailAndPassword(values.email, values.password);
+      let userCred = null;
+      try {
+        userCred = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password);
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = "bg-red-500";
+        this.reg_alert_msg = "An unexpected error occurred. Please try again.";
+        return;
+      }
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Your account has been created successfully.";
-      console.log(values);
+      console.log(userCred);
     },
   },
 };
